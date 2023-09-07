842aac78b9d39cb133d2c92c5b8901059a40548d
Luoyan Zhong <115356857+CreepingZhong@users.noreply.github.com>
2023-09-07 12:30:48 -0400

    Update data_collection.py

--- wholearm_ws/wholearm_ws/src/wholearm_skin_ros/scripts/data_collection.py
+++ wholearm_ws/wholearm_ws/src/wholearm_skin_ros/scripts/data_collection.py
@@ -15,7 +15,7 @@ import threading
 # Declraing a lock
 lock = threading.Lock()
 taxel = 0
-filename = "0517_noshield_12"
+filename = "compress0"
 
 from std_srvs.srv import SetBool, SetBoolResponse
 
